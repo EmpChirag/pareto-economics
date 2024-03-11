1b56693865aa69b5f0b309b4aeca9e80f72c4814
tulsiempyreal <tulsi.empyreal@gmail.com>
2024-03-11 18:12:23 +0530

    add browserrouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import routes from './routes';
 
 function App() {
   return (
-    <div>Test</div>
+    <BrowserRouter basename="/">
+    <Routes>
+      {routes.map((route, index) => (
+        <Route
+          key={index}
+          exact={route.exact}
+          path={route.path}
+          element={route.component}
+        />
+      ))}
+    </Routes>
+  </BrowserRouter>
   );
 }
 
