494eba30bb327ed92dbb18d0ed179d7d08dfaf98
tulsiempyreal <tulsi.empyreal@gmail.com>
2024-03-11 19:21:12 +0530

    create about page

--- src/pages/AboutUs/index.js
+++ src/pages/AboutUs/index.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div className='container max-width-[1480px] mx-auto'>
+      <h2 className='text-[34px]'>Our Core</h2>
+     <div className='border-b border-slate-950 pb-[10px]'>
+      <img  src='images/AboutUs/Our-Core.png' className='h-[478px] w-full'/>
+     </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+      "2xl": "1480px",
+      "max-2xl": { max: "1439px" },
+      "max-xl": { max: "1279px" },
+      "max-1150": { max: "1149px" },
+      "max-lg": { max: "1023px" },
+      "max-900": { max: "899px" },
+      "max-md": { max: "767px" },
+      "max-sm": { max: "639px" }
+    }}
   },
   plugins: []
 };
