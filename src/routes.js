d4d751fff12f8a95942e901f61c7cfd88c4865c9
Nitesh Parmar <nitesh.empyreal@gmail.com>
2024-03-12 15:14:31 +0530

    added global power index page, client corporation page, dark footer

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import React, { lazy } from "react";
 const Home = lazy(() => import("./pages/Home")
 );
 const AboutUs = lazy(()=>import("./pages/AboutUs"));
+const GlobalPowerIndex = lazy(() => import("./pages/GlobalPowerIndex"));
+const ClientsCorporations = lazy(() => import("./pages/ClientsCorporations"));
 
 const routes = [
   {
@@ -13,9 +15,19 @@ const routes = [
   },
   {
     path: "/about-us",
-    component: <AboutUs/>,
+    component: <AboutUs />,
     exact: true
   },
+  {
+    path: "/global-power-index",
+    component: <GlobalPowerIndex />,
+    exact: true
+  },
+  {
+    path: "/clients-corporations",
+    component: <ClientsCorporations />,
+    exact: true
+  }
 ];
 
 export default routes;
