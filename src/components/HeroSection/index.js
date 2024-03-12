a92e5f4670286b6dfc3e5666813c254bab61f46a
empchirag <chirag.gusani@empyreal.com>
2024-03-12 11:20:06 +0530

    h1 width change condition

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-const HeroSection = ({ className, title, image }) => {
+const HeroSection = ({ className, title, image, h1width }) => {
   return (
     <section
       className={`hero ${className} mt-10 pb-10 border-black border-b-2`}
     >
-      <h1 className="text-4xl leading-[1.2] lg:text-6xl font-semibold mb-6 max-w-[500px]">
+      <h1
+        className={`text-4xl leading-[1.2] lg:text-6xl font-semibold mb-6 ${
+          h1width ? h1width : "max-w-[500px]"
+        }`}
+      >
         {title}
       </h1>
       <img src={image} alt="Hero Section" height="1480" width="492" />
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -40,6 +40,7 @@ const Home = () => {
       <DiscoverPowerIndex />
       <OurClients />
       <ParetoThoughtLeadership />
+
       <Footer />
     </div>
   );
