8a2b063f2ec97da589cfdb7246652e684ac31592
= <nitesh.empyreal@gmail.com>
2024-03-13 20:39:22 +0530

    add .contacus

--- src/pages/ContactUs/index.js
+++ src/pages/ContactUs/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ParetoEconomics from "../../assets/images/pareto-economics-logo.png";
-
+import Logo from "../../assets/images/pareto-logo.webp";
 const ContactUs = () => {
   return (
     <div>
@@ -11,7 +10,7 @@ const ContactUs = () => {
             <img src={Logo} alt="Pareto Economics" width="90%" height="auto" />
           </Link>
         </div>
-        <div className="md:col-span-3 text-white bg-black">
+        <div className="col-span-3 text-white bg-black">
           <div className="text-end mt-8 mb-5">
             <Link className="text-4xl mr-20 max-sm:mr-12" to="/">
               X
@@ -22,94 +21,94 @@ const ContactUs = () => {
               <div className="mt-20 mb-20 text-white">
                 <p>CONTACT / DASHBOARD DEMO / PARTNERSHIP INQUIRY</p>
               </div>
-              <div className="mb-5 group">
-                <label htmlFor="fname" className="text-xl uppercase">
-                  First Name
-                  <span className="text-red-600 ml-1">*</span>
+              <div className="relative z-0 w-full mb-5 group">
+                <label htmlFor="fname" className="text-2xl uppercase">
+                  first name
+                  <span className="text-red-600 ms-4">*</span>
                 </label>
                 <input
                   type="text"
                   name="fname"
                   id="fname"
-                  className="block w-full text-xl border-b border-white bg-transparent appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:border-blue-600 peer"
-                  placeholder=""
+                  className="block px-0 w-full text-2xl bg-transparent border-0 border-b  appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
                   required
                 />
               </div>
-              <div className="mb-5 group">
-                <label htmlFor="lname" className="text-xl uppercase">
+              <div className="relative z-0 w-full mb-5 group">
+                <label htmlFor="lname" className="text-2xl uppercase">
                   last name
-                  <span className="text-red-600 ml-1">*</span>
+                  <span className="text-red-600 ms-4">*</span>
                 </label>
                 <input
                   type="text"
                   name="lname"
                   id="lname"
-                  className="block w-full text-xl border-b border-white bg-transparent appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:border-blue-600 peer"
-                  placeholder=""
+                  className="block px-0 w-full text-2xl bg-transparent border-0 border-b  appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
                   required
                 />
               </div>
-              <div className="mb-5 group">
-                <label htmlFor="company" className="text-xl uppercase">
+              <div className="relative z-0 w-full mb-5 group">
+                <label htmlFor="company" className="text-2xl uppercase">
                   company/organisation
-                  <span className="text-red-600 ml-1">*</span>
+                  <span className="text-red-600 ms-4">*</span>
                 </label>
                 <input
                   type="text"
                   name="company"
                   id="company"
-                  className="block w-full text-xl border-b border-white bg-transparent appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:border-blue-600 peer"
-                  placeholder=""
+                  className="block px-0 w-full text-2xl bg-transparent border-0 border-b  appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
                   required
                 />
               </div>
-              <div className="mb-5 group">
-                <label htmlFor="jobtitle" className="text-xl uppercase">
-                  Job title
-                  <span className="text-red-600 ml-1">*</span>
+              <div className="relative z-0 w-full mb-5 group">
+                <label htmlFor="jobtitle" className="text-2xl uppercase">
+                  job title
+                  <span className="text-red-600 ms-4">*</span>
                 </label>
                 <input
                   type="text"
                   name="jobtitle"
                   id="jobtitle"
-                  className="block w-full text-xl border-b border-white bg-transparent appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:border-blue-600 peer"
-                  placeholder=""
+                  className="block px-0 w-full text-2xl bg-transparent border-0 border-b  appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
                   required
                 />
               </div>
-              <div className="mb-5 group">
-                <label htmlFor="email" className="text-xl uppercase">
-                  Business Email
-                  <span className="text-red-600 ml-1">*</span>
+              <div className="relative z-0 w-full mb-5 group">
+                <label htmlFor="email" className="text-2xl uppercase">
+                  business email
+                  <span className="text-red-600 ms-4">*</span>
                 </label>
                 <input
                   type="email"
                   name="email"
                   id="email"
-                  className="block w-full text-xl border-b border-white bg-transparent appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:border-blue-600 peer"
-                  placeholder=""
+                  className="block px-0 w-full text-2xl bg-transparent border-0 border-b  appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
                   required
                 />
               </div>
-              <div className="mb-5 group">
-                <label htmlFor="phone" className="text-xl uppercase">
-                  Phone Number
-                  <span className="text-red-600 ml-1">*</span>
+              <div className="relative z-0 w-full mb-5 group">
+                <label htmlFor="phone" className="text-2xl uppercase">
+                  phone number
+                  <span className="text-red-600 ms-4">*</span>
                 </label>
                 <input
                   type="number"
                   name="phone"
                   id="phone"
-                  className="block w-full text-xl border-b border-white bg-transparent appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:border-blue-600 peer"
-                  placeholder=""
+                  className="block px-0 w-full text-2xl bg-transparent border-0 border-b  appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
                   required
                 />
               </div>
-              <div className="mb-5 group">
-                <label htmlFor="phone" className="text-xl uppercase">
-                  Topic
-                  <span className="text-red-600 ml-1">*</span>
+              <div className="relative z-0 w-full mb-5 group">
+                <label htmlFor="topic" className="text-2xl uppercase">
+                  topic
+                  <span className="text-red-600 ms-4">*</span>
                 </label>
                 <div className="flex">
                   <ul>
@@ -166,30 +165,29 @@ const ContactUs = () => {
                   </ul>
                 </div>
                 <input
-                  type="text"
-                  name="topic"
-                  id="topic"
-                  className="block w-full text-xl border-b border-white bg-transparent appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:border-blue-600 peer"
-                  placeholder=""
+                  type="number"
+                  name="phone"
+                  id="phone"
+                  className="block px-0 w-full text-2xl bg-transparent border-0 border-b  appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
                   required
                 />
               </div>
-              <div className="mb-5 group">
-                <label
-                  htmlFor="message"
-                  className="block mb-2 text-xl font-medium uppercase"
-                >
-                  Tell Us More...
-                </label>
-                <textarea
-                  id="message"
-                  rows="4"
-                  className="block w-full p-2.5 text-xl border-white border bg-black"
-                />
-              </div>
+
+              <label
+                htmlFor="message"
+                className="block mb-2 text-2xl font-medium uppercase"
+              >
+                tell us more...
+              </label>
+              <textarea
+                id="message"
+                rows="4"
+                className="block p-2.5 w-full text-2xl border-white border-2 bg-black"
+              />
               <button
                 type="submit"
-                className="mt-8 w-80 h-12 border border-white hover:bg-white hover:text-black text-xl"
+                className=" mt-8 border-2 border-white w-80 h-12 px-6 hover:bg-white hover:text-black text-xl"
               >
                 Submit
               </button>
@@ -199,11 +197,12 @@ const ContactUs = () => {
                   <Link>
                     <i className="border-white border-b mx-2">Privacy Policy</i>
                   </Link>
-                  regarding how we will handle this information.
+                  regarding how we will
+                  <br /> handle this information.
                 </p>
               </div>
               <div className="mt-10 mb-10">
-                <h3 className="">UK Office Number</h3>
+                <h3 className="text-bold">UK Office Number</h3>
                 <p className="mt-3">+44 (0) 20 8154 3425</p>
               </div>
             </form>
